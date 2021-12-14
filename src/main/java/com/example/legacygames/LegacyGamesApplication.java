06b003d5ba0724a43445336c743fe7042b7b888f
varanitsa <varanitsa.es@gmail.com>
2021-12-14 09:49:17 +0100

    creando proyecto

---
+++ src/main/java/com/example/legacygames/LegacyGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.legacygames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/legacygames/LegacyGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legacygames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
