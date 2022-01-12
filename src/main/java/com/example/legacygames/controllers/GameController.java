993d19b83cb14f1e29a1490fb864e9aebe2040a3
AnnaGirona89 <annartworks@gmail.com>
2022-01-12 14:06:04 +0100

    Rs

--- src/main/java/com/example/legacygames/controllers/GameController.java
+++ src/main/java/com/example/legacygames/controllers/GameController.java
@@ -30,7 +30,6 @@ public class GameController {
     String getForm(Model model){
 
         Game game = new Game();
-
         model.addAttribute("game", game);
         model.addAttribute("title", "Game list");
 
